86521cf4add1afedd653353de93b47196d705036
Anton Voshchinskiy <anton.vosc@gmail.com>
2026-02-02 20:34:52 +0000

    Downloaded music files locally and completed setup for project routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { MusicPlayer } from "./components/MusicPlayer";
+import { AllSongs } from "./components/AllSongs";
+import { Playlists } from "./components/Playlists";
+import { BrowserRouter, Routes, Route } from "react-router";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <div className="app">
+        {/* <Navbar /> */}
+
+        <main className="app-main">
+          <div className="player-section">
+            <MusicPlayer />
+          </div>
+
+          <div className="content-section">
+            <Routes>
+              <Route path="/" element={<AllSongs />} />
+
+              <Route path="/playlists" element={<Playlists />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
